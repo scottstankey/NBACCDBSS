e6c6ab947f1ce2969aabd424247c5b8ee859db98
scottstankey <scott.stankey@me.com>
2016-01-18 18:31:49 -0500

    Finished Average Points

--- FeatureFunctions/AveragePoints.R
+++ FeatureFunctions/AveragePoints.R
@@ -1,7 +1,12 @@
-ScoringWindowAverage = function(days = 10, oneseason = T, data = allseasons, player = "201149", gamedate = "20151219", removeifless = F) {
+ScoringWindowAverage = function(days = 10, oneseason = T, data = allseasons, 
+                                player = "201149", gamedate = "20151219", 
+                                removeifless = F)
+{
   inds = intersect(which(data$PLAYER_ID == player), which(data$GAME_DATE < gamedate))
+  year = substr(gamedate,4,4)
+  inds = if(oneseason == T) {intersect(which(substr(data$SEASON_ID,5,5) == year),inds)}
   tmp = data[inds,]
   tmp = tmp[order(tmp$GAME_DATE, decreasing = T),]
-  if(removeifless = F &&)
-  return(sum(tmp[1:days,"PTS"]) / days)
+  outp = ifelse((removeifless = T && nrow(tmp) < days),NA,sum(tmp[1:days,"PTS"]) / days)
+  return(outp)
 }
