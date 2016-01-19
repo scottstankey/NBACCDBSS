6f68bcea4ee53da58ef774e126e7a556f931a888
scottstankey <scott.stankey@me.com>
2016-01-18 19:43:27 -0500

    Streamline = get function from separate file

--- FeatureFunctions/AveragePoints.R
+++ FeatureFunctions/AveragePoints.R
@@ -5,15 +5,11 @@ ScoringWindowAverage = function(days = 10, oneseason = T, data = allseasons,
                                 removeifless = F, ewma = T, ewmalookback = 5, onlyhomeoraway = T, home = 1,
                                 oneopponent = F, opponent = "BOS")
 {
-  inds = intersect(which(data$PLAYER_ID == player), which(data$GAME_DATE < gamedate))
-  if(oneseason == T)
-    {
-    inds = intersect(which(data$SEASON_ID == seasonid),inds)
-    }
-  if(onlyhomeoraway == T){inds = intersect(which(data$HOME == home),inds)}
-  if(oneopponent == T){intersect(which(data$OPPONENT == opponent),inds)}
-  tmp = data[inds,]
-  tmp = tmp[order(tmp$GAME_DATE, decreasing = T),]
+  source("/Users/scottstankey/GitHub/NBACCDBSS/FeatureFunctions/SelectGamesHelper.R")
+  tmp = SelectGames(days, oneseason, data, 
+                  player, gamedate, seasonid,
+                  removeifless, onlyhomeoraway, home,
+                  oneopponent, opponent)
   outp = ifelse((removeifless == T && nrow(tmp) < days),
                 NA,
                 ifelse(ewma == T,
