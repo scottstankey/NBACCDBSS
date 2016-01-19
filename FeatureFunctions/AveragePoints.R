5ec87f724424bebeabb001c8c9c9957fa742198c
scottstankey <scott.stankey@me.com>
2016-01-18 19:23:02 -0500

    More Points Features

--- FeatureFunctions/AveragePoints.R
+++ FeatureFunctions/AveragePoints.R
@@ -1,12 +1,22 @@
+#requires library TTR
+
 ScoringWindowAverage = function(days = 10, oneseason = T, data = allseasons, 
-                                player = "201149", gamedate = "20151219", 
-                                removeifless = F)
+                                player = "201149", gamedate = "20151219", season = "2015-16",
+                                removeifless = F, ewma = T, ewmalookback = 5, onlyhomeoraway = T, home = 1,
+                                oneopponent = F, opponent = "BOS")
 {
   inds = intersect(which(data$PLAYER_ID == player), which(data$GAME_DATE < gamedate))
-  year = substr(gamedate,4,4)
-  inds = if(oneseason == T) {intersect(which(substr(data$SEASON_ID,5,5) == year),inds)}
+  year = substr(season,4,4)
+  if(oneseason == T)
+    {
+    inds = intersect(which(substr(data$SEASON_ID,5,5) == year),inds)
+    }
+  if(onlyhomeoraway == T){inds = intersect(which(data$HOME == home),inds)}
+  if(oneopponent == T){intersect(which(data$OPPONENT == opponent),inds)}
   tmp = data[inds,]
   tmp = tmp[order(tmp$GAME_DATE, decreasing = T),]
-  outp = ifelse((removeifless = T && nrow(tmp) < days),NA,sum(tmp[1:days,"PTS"]) / days)
+  outp = ifelse((removeifless == T && nrow(tmp) < days),NA,
+                ifelse(ewma == T,EMA(tmp[1:days,"PTS"], n = ewmalookback)[days],
+                      sum(tmp[1:days,"PTS"] / days)))
   return(outp)
 }
