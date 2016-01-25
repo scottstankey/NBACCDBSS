045e28a120b576cf1ce69de0f5febf75d3dc0d89
davidbrandfonbrener <David@Macintosh-3.local>
2016-01-24 21:03:29 -0500

    team and opponent stats

--- FeatureFunctions/opponent.R
+++ FeatureFunctions/opponent.R
@@ -1,14 +1,14 @@
-
-PaceWindowAverage = function(days = 10, oneseason = T, data = allseasons, 
-                                player = "201149", team = "CHI", gamedate = "20151219", season_ID = "22015"
-                                removeifless = F)
+OppPaceWindowAverage = function(days = 100, oneseason = T, data = allseasons, 
+                             player = "201149", team = "CHI", opp = "UTA", gamedate = "20151219", season_ID = "22015",
+                             removeifless = F)
 {
+  oppplayer = data[which(data$OPPONENT == opp), ]$PLAYER_ID[1]
   #games to search
   source("~/NBACCDBSS/FeatureFunctions/SelectGamesHelper.R")
   data2 = SelectGames(days, oneseason, data, 
-                      player, gamedate, season_ID,
+                      oppplayer, gamedate, season_ID,
                       removeifless, F, 1,
-                       F, "BOS")
+                      F, "BOS")
   data3 = data2$GAME_ID
   
   #find all other players in those games
@@ -16,20 +16,23 @@ PaceWindowAverage = function(days = 10, oneseason = T, data = allseasons,
   
   teamGames = unique(tmp$GAME_ID)
   possessions = NULL
+  drating = NULL
   
   for(game in teamGames)
   {
     playersInGame = which(tmp$GAME_ID == game)
     possessionsInGame = 0
+    points = 0
     for(p in playersInGame)
     {
       possessionsInGame = possessionsInGame + tmp$FGA[p] + tmp$TOV[p] + (.44 * tmp$FTA[p]) - tmp$OREB[p]
+      points = points + tmp$PTS[p]
     }
     possessions = c(possessions, possessionsInGame)
+    orating = c(orating, (points / possessionsInGame))
   }
   
   outp = mean(possessions) / 2
-    
+  
   return(outp)
 }
-
--- FeatureFunctions/team.R
+++ FeatureFunctions/team.R
@@ -1,14 +1,14 @@
-OppPaceWindowAverage = function(days = 100, oneseason = T, data = allseasons, 
-                             player = "201149", team = "CHI", opp = "UTA", gamedate = "20151219", season_ID = "22015",
-                             removeifless = F)
+
+PaceWindowAverage = function(days = 10, oneseason = T, data = allseasons, 
+                                player = "201149", team = "CHI", gamedate = "20151219", season_ID = "22015"
+                                removeifless = F)
 {
-  oppplayer = data[which(data$OPPONENT == opp), ]$PLAYER_ID[1]
   #games to search
   source("~/NBACCDBSS/FeatureFunctions/SelectGamesHelper.R")
   data2 = SelectGames(days, oneseason, data, 
-                      oppplayer, gamedate, season_ID,
+                      player, gamedate, season_ID,
                       removeifless, F, 1,
-                      F, "BOS")
+                       F, "BOS")
   data3 = data2$GAME_ID
   
   #find all other players in those games
@@ -16,19 +16,24 @@ OppPaceWindowAverage = function(days = 100, oneseason = T, data = allseasons,
   
   teamGames = unique(tmp$GAME_ID)
   possessions = NULL
+  orating = NULL
   
   for(game in teamGames)
   {
     playersInGame = which(tmp$GAME_ID == game)
     possessionsInGame = 0
+    points = 0
     for(p in playersInGame)
     {
       possessionsInGame = possessionsInGame + tmp$FGA[p] + tmp$TOV[p] + (.44 * tmp$FTA[p]) - tmp$OREB[p]
+      points = points + tmp$PTS[p]
     }
     possessions = c(possessions, possessionsInGame)
+    orating = c(orating, (points / possessionsInGame))
   }
   
   outp = mean(possessions) / 2
-  
+    
   return(outp)
 }
+
